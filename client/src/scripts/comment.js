9770e4663e4e1f95185158905c334b0e05aa6085
sebelga <sabee77@gmail.com>
2018-01-27 13:01:50 +0100

    feat(comments): delete comment

--- client/src/scripts/comment.js
+++ client/src/scripts/comment.js
@@ -11,7 +11,7 @@ let commentFormErrorsDOM;
 const deleteComment = e => {
     const id = parseInt(e.currentTarget.dataset.commentId, 10);
 
-    axios
+    return axios
         .delete(`${apiBase}/comments/${id}`)
         .then(() => {
             const commentNode = document.getElementById(`comment-${id}`);
@@ -21,15 +21,12 @@ const deleteComment = e => {
 };
 
 const loadMoreComments = id => {
-    axios
+    return axios
         .get(`${apiBase}/blog/${id}/comments?start=${nextPage}`)
         .then(response => {
             if (response.data.entities) {
                 response.data.entities.forEach(comment => {
                     commentsDOM.append(commentNode(comment));
-                    // $(`#comment-${comment.id}`)
-                    //     .find("button.delete")
-                    //     .click(deleteComment);
                 });
 
                 if (
@@ -39,7 +36,9 @@ const loadMoreComments = id => {
                     nextPage = response.data.nextPageCursor;
                 } else {
                     // No more comments (hide button)
-                    document.querySelector('#load-more-comments button').classList.add('is-hidden');
+                    document
+                        .querySelector("#load-more-comments button")
+                        .classList.add("is-hidden");
                 }
             }
         })
@@ -50,7 +49,9 @@ const loadMoreComments = id => {
 
 const submitComment = (e, id) => {
     e.preventDefault();
-    commentFormErrorsDOM.classList.add('is-hidden');
+
+    commentFormErrorsDOM.classList.add("is-hidden");
+
     const postId = parseInt(e.target.dataset.postId, 10);
     const name = e.target.elements[0].value;
     const website = e.target.elements[1].value || null;
@@ -63,33 +64,27 @@ const submitComment = (e, id) => {
         comment
     };
 
-    axios
+    return axios
         .post(`${apiBase}/blog/${postId}/comments`, data)
         .then(response => {
-            commentsDOM.insertBefore(commentNode(response.data), commentsDOM.firstChild);
+            commentsDOM.insertBefore(
+                commentNode(response.data),
+                commentsDOM.firstChild
+            );
             commentFormDOM.reset();
-            // commentsDOM.prepend(commentNode(response.data));
-
-            // const btn = document.querySelector(
-            //     `#comment-${response.data.id} .comment-delete`
-            // );
-            // btn.addEventListener("click", deleteComment);
-            // // $(`#comment-${response.data.id}`)
-            // //     .find("button.delete")
-            // //     .click(deleteComment);
         })
         .catch(res => {
             const error = res.response.data;
             let message;
-            if (error.name == 'ValidationError') {
+            if (error.name == "ValidationError") {
                 message = error.details.reduce((acc, err) => {
                     acc += `${err.message}<br>`;
                     return acc;
-                }, '');
+                }, "");
             } else {
                 message = error.message;
             }
-            commentFormErrorsDOM.classList.remove('is-hidden');
+            commentFormErrorsDOM.classList.remove("is-hidden");
             commentFormErrorsDOM.innerHTML = message;
         });
 };
@@ -99,24 +94,20 @@ const commentNode = entity => {
 
     const node = document.createElement("article");
     node.id = `comment-${id}`;
-    node.className = "media"
+    node.className = "media";
     node.innerHTML = `
-        <div class="media-content">
+        <div class="media-content comment">
             <div class="content">
                 <p>
                     <strong>${name}</strong> <small>wrote ${createdOnAgo}</small><br>
                     ${comment}
                 </p>
+                <button class="delete" data-comment-id="${id}"></delete>
             </div>
         </div>
     `;
 
-    // const buttonNode = document.createElement("button");
-    // buttonNode.classList.add("comment-delete");
-    // buttonNode.dataset.commentId = id;
-    // buttonNode.innerHTML = '<i class="fa fa-close"></i>';
-    // buttonNode.addEventListener("click", deleteComment);
-    // node.appendChild(buttonNode);
+    node.querySelector(".delete").addEventListener("click", deleteComment);
 
     return node;
 };
@@ -126,19 +117,23 @@ const pageReady = pageId => {
         blogPostId = window.__blogPostData.id;
         nextPage = window.__blogPostData.nextPage;
         commentsDOM = document.getElementById("comments-wrapper");
-        commentFormErrorsDOM = document.getElementById('comment-form-errors');
+        commentFormErrorsDOM = document.getElementById("comment-form-errors");
 
         commentFormDOM = document.getElementById("form-comment");
         commentFormDOM.addEventListener("submit", submitComment);
 
         const buttonsDeleteComments = Array.prototype.slice.apply(
-            document.querySelectorAll(".comment-delete")
+            document.querySelectorAll(".comment .delete")
         );
-        buttonsDeleteComments.forEach(el => el.addEventListener("click", deleteComment));
+        buttonsDeleteComments.forEach(el =>
+            el.addEventListener("click", deleteComment)
+        );
 
-        const btnLoadMore = document.getElementById('load-more-comments');
+        const btnLoadMore = document.getElementById("load-more-comments");
         if (btnLoadMore) {
-            btnLoadMore.addEventListener('click', () => loadMoreComments(blogPostId));
+            btnLoadMore.addEventListener("click", () =>
+                loadMoreComments(blogPostId)
+            );
         }
     }
 };
--- server/public/js/main.js
+++ server/public/js/main.js
@@ -20297,7 +20297,7 @@ let commentFormErrorsDOM;
 const deleteComment = e => {
     const id = parseInt(e.currentTarget.dataset.commentId, 10);
 
-    __WEBPACK_IMPORTED_MODULE_0_axios___default.a
+    return __WEBPACK_IMPORTED_MODULE_0_axios___default.a
         .delete(`${__WEBPACK_IMPORTED_MODULE_1__config__["a" /* apiBase */]}/comments/${id}`)
         .then(() => {
             const commentNode = document.getElementById(`comment-${id}`);
@@ -20307,15 +20307,12 @@ const deleteComment = e => {
 };
 
 const loadMoreComments = id => {
-    __WEBPACK_IMPORTED_MODULE_0_axios___default.a
+    return __WEBPACK_IMPORTED_MODULE_0_axios___default.a
         .get(`${__WEBPACK_IMPORTED_MODULE_1__config__["a" /* apiBase */]}/blog/${id}/comments?start=${nextPage}`)
         .then(response => {
             if (response.data.entities) {
                 response.data.entities.forEach(comment => {
                     commentsDOM.append(commentNode(comment));
-                    // $(`#comment-${comment.id}`)
-                    //     .find("button.delete")
-                    //     .click(deleteComment);
                 });
 
                 if (
@@ -20325,7 +20322,9 @@ const loadMoreComments = id => {
                     nextPage = response.data.nextPageCursor;
                 } else {
                     // No more comments (hide button)
-                    document.querySelector('#load-more-comments button').classList.add('is-hidden');
+                    document
+                        .querySelector("#load-more-comments button")
+                        .classList.add("is-hidden");
                 }
             }
         })
@@ -20336,7 +20335,9 @@ const loadMoreComments = id => {
 
 const submitComment = (e, id) => {
     e.preventDefault();
-    commentFormErrorsDOM.classList.add('is-hidden');
+
+    commentFormErrorsDOM.classList.add("is-hidden");
+
     const postId = parseInt(e.target.dataset.postId, 10);
     const name = e.target.elements[0].value;
     const website = e.target.elements[1].value || null;
@@ -20349,33 +20350,27 @@ const submitComment = (e, id) => {
         comment
     };
 
-    __WEBPACK_IMPORTED_MODULE_0_axios___default.a
+    return __WEBPACK_IMPORTED_MODULE_0_axios___default.a
         .post(`${__WEBPACK_IMPORTED_MODULE_1__config__["a" /* apiBase */]}/blog/${postId}/comments`, data)
         .then(response => {
-            commentsDOM.insertBefore(commentNode(response.data), commentsDOM.firstChild);
+            commentsDOM.insertBefore(
+                commentNode(response.data),
+                commentsDOM.firstChild
+            );
             commentFormDOM.reset();
-            // commentsDOM.prepend(commentNode(response.data));
-
-            // const btn = document.querySelector(
-            //     `#comment-${response.data.id} .comment-delete`
-            // );
-            // btn.addEventListener("click", deleteComment);
-            // // $(`#comment-${response.data.id}`)
-            // //     .find("button.delete")
-            // //     .click(deleteComment);
         })
         .catch(res => {
             const error = res.response.data;
             let message;
-            if (error.name == 'ValidationError') {
+            if (error.name == "ValidationError") {
                 message = error.details.reduce((acc, err) => {
                     acc += `${err.message}<br>`;
                     return acc;
-                }, '');
+                }, "");
             } else {
                 message = error.message;
             }
-            commentFormErrorsDOM.classList.remove('is-hidden');
+            commentFormErrorsDOM.classList.remove("is-hidden");
             commentFormErrorsDOM.innerHTML = message;
         });
 };
@@ -20385,24 +20380,20 @@ const commentNode = entity => {
 
     const node = document.createElement("article");
     node.id = `comment-${id}`;
-    node.className = "media"
+    node.className = "media";
     node.innerHTML = `
-        <div class="media-content">
+        <div class="media-content comment">
             <div class="content">
                 <p>
                     <strong>${name}</strong> <small>wrote ${createdOnAgo}</small><br>
                     ${comment}
                 </p>
+                <button class="delete" data-comment-id="${id}"></delete>
             </div>
         </div>
     `;
 
-    // const buttonNode = document.createElement("button");
-    // buttonNode.classList.add("comment-delete");
-    // buttonNode.dataset.commentId = id;
-    // buttonNode.innerHTML = '<i class="fa fa-close"></i>';
-    // buttonNode.addEventListener("click", deleteComment);
-    // node.appendChild(buttonNode);
+    node.querySelector(".delete").addEventListener("click", deleteComment);
 
     return node;
 };
@@ -20412,19 +20403,23 @@ const pageReady = pageId => {
         blogPostId = window.__blogPostData.id;
         nextPage = window.__blogPostData.nextPage;
         commentsDOM = document.getElementById("comments-wrapper");
-        commentFormErrorsDOM = document.getElementById('comment-form-errors');
+        commentFormErrorsDOM = document.getElementById("comment-form-errors");
 
         commentFormDOM = document.getElementById("form-comment");
         commentFormDOM.addEventListener("submit", submitComment);
 
         const buttonsDeleteComments = Array.prototype.slice.apply(
-            document.querySelectorAll(".comment-delete")
+            document.querySelectorAll(".comment .delete")
         );
-        buttonsDeleteComments.forEach(el => el.addEventListener("click", deleteComment));
+        buttonsDeleteComments.forEach(el =>
+            el.addEventListener("click", deleteComment)
+        );
 
-        const btnLoadMore = document.getElementById('load-more-comments');
+        const btnLoadMore = document.getElementById("load-more-comments");
         if (btnLoadMore) {
-            btnLoadMore.addEventListener('click', () => loadMoreComments(blogPostId));
+            btnLoadMore.addEventListener("click", () =>
+                loadMoreComments(blogPostId)
+            );
         }
     }
 };
